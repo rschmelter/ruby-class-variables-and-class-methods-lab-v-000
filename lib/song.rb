644dc116ae4f34b7ac67824e7daff7d13b007c8a
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-11 16:31:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,21 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@count = 0 
+  @@count = 0
+  @@genres = []
 
   def initialize(name, artist, genre)
     @@count += 1
+    unless @@genres.include?(genre)
+      @@genres << genre
+    end
   end
 
   def self.count
     @@count
+  end
+
+  def self.genres
+    
   end
 
 end
